47ad7cdfdd6fd35e360fcfc442b2edf9f4b29c15
Amy Stockhausen <stockhausenamy@gmail.com>
2023-01-30 20:28:36 -0600

    Got a little further.

--- src/main/java/quizbuilder/Quiz.java
+++ src/main/java/quizbuilder/Quiz.java
@@ -1,9 +1,19 @@
 package quizbuilder;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+import quizbuilder.*;
 
 public class Quiz {
 
+    Scanner input = new Scanner(System.in);
 
+    private ArrayList<Object> questionBank = new ArrayList<Object>();
+    Double rawPoints = 0.0;
 
+    public void main() {
+        for (Object singleQuestion : questionBank) {
+
+        }
+    }
 }
